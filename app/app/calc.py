c565d4ba396b2dc53add3d5830bef490014033f2
Anshul Gupta <agupta@akamai.com>
2023-06-10 13:58:54 +0530

    TDD trial

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x,y):
+    """Add x & y and return result"""
+    return x + y
+
+def sub(x,y):
+    """Sub y from x and return result"""
+    return x - y
